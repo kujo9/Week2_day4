a4094c3d44c405506855dd00e9d9bb4cc9d5507c
kujo9 <avossanespoir@gmail.com>
2023-10-23 08:15:36 -0500

    first

---
+++ module.py
@@ -0,0 +1,9 @@
+def printName(name):
+    print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+    
+def Square(length, width):
+    return length * width
+
+def circumference(radius):
+    import math 
+    return 2 * math.pi * radius
